489da52e9e138954203ac97932d7c54342b53a85
청수 <bestowing@naver.com>
2022-06-21 22:21:25 +0900

    test: 채팅방추가 뷰모델 테스트

---
+++ Virtual-Korea-ViewModelTests/Mock/MockChatRoomUsecase.swift
@@ -0,0 +1,41 @@
+//
+//  MockChatRoomUsecase.swift
+//  Virtual-Korea-ViewModelTests
+//
+//  Created by 이청수 on 2022/06/08.
+//
+
+import Domain
+import RxSwift
+
+final class MockChatRoomUsecase: Domain.ChatRoomsUsecase {
+
+    let chatRoomsStream: Observable<ChatRoom>
+    let createChatRoomStream: Observable<Void>
+    let latestChatStream: Observable<Chat>
+    let userNumberStream: Observable<UInt>
+
+    init() {
+        self.chatRoomsStream = PublishSubject<ChatRoom>.init()
+        self.createChatRoomStream = PublishSubject<Void>.init()
+        self.latestChatStream = PublishSubject<Chat>.init()
+        self.userNumberStream = PublishSubject<UInt>.init()
+    }
+
+    func chatRooms() -> Observable<ChatRoom> {
+        self.chatRoomsStream
+    }
+
+    func create(chatRoom: ChatRoom) -> Observable<Void> {
+        self.createChatRoomStream
+    }
+    
+    func latestChat(chatRoomID: String) -> Observable<Chat> {
+        self.latestChatStream
+    }
+    
+    func numberOfUsers(chatRoomID: String) -> Observable<UInt> {
+        self.userNumberStream
+    }
+
+}
---
+++ Virtual-Korea-ViewModelTests/Mock/MockUserInfoUsecase.swift
@@ -0,0 +1,71 @@
+//
+//  MockUserInfoUsecase.swift
+//  Virtual-Korea-ViewModelTests
+//
+//  Created by 이청수 on 2022/06/08.
+//
+
+import Domain
+import RxSwift
+
+final class MockUserInfoUsecase: Domain.UserInfoUsecase {
+
+    let addEventStream: PublishSubject<Void>
+    let addSideEventStream: PublishSubject<Void>
+    let addUserInfoStream: PublishSubject<Void>
+    let clearSideStream: PublishSubject<Void>
+    let voteStream: PublishSubject<Void>
+    let uidStream: PublishSubject<String>
+    let roomUserInfoStream: PublishSubject<UserInfo?>
+    let connectStream: PublishSubject<UserInfo>
+    let userInfoStream: PublishSubject<UserInfo?>
+
+    init() {
+        self.addEventStream = PublishSubject<Void>.init()
+        self.addSideEventStream = PublishSubject<Void>.init()
+        self.addUserInfoStream = PublishSubject<Void>.init()
+        self.clearSideStream = PublishSubject<Void>.init()
+        self.voteStream = PublishSubject<Void>.init()
+        self.uidStream = PublishSubject<String>.init()
+        self.roomUserInfoStream = PublishSubject<UserInfo?>.init()
+        self.connectStream = PublishSubject<UserInfo>.init()
+        self.userInfoStream = PublishSubject<UserInfo?>.init()
+    }
+
+    func add(roomID: String, userID: String) -> Observable<Void> {
+        self.addEventStream
+    }
+
+    func add(roomID: String, userID: String, side: Side) -> Observable<Void> {
+        self.addSideEventStream
+    }
+
+    func add(userInfo: UserInfo) -> Observable<Void> {
+        self.addUserInfoStream
+    }
+
+    func clearSide(roomID: String, userID: String) -> Observable<Void> {
+        self.clearSideStream
+    }
+
+    func vote(roomID: String, userID: String, side: Side) -> Observable<Void> {
+        self.voteStream
+    }
+
+    func uid() -> Observable<String> {
+        self.uidStream
+    }
+
+    func userInfo(roomID: String, with userID: String) -> Observable<UserInfo?> {
+        self.roomUserInfoStream
+    }
+
+    func connect(roomID: String) -> Observable<UserInfo> {
+        self.connectStream
+    }
+
+    func userInfo(userID: String) -> Observable<UserInfo?> {
+        self.userInfoStream
+    }
+
+}
